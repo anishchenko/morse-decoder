3dfd8fac23a11fc0847c6229647c08ee3a812b96
oleksandr anishchenko <anishchenko.alexandr@gmail.com>
2023-05-06 20:10:52 +0300

    add solutions

--- src/index.js
+++ src/index.js
@@ -39,6 +39,119 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    // ----------- variant 2 ---------------------
+    const exprArr = Array.from(expr);
+    
+    const digitSymbolsArr = breakExprToSymbols(exprArr);
+    const morseSymbolsArr = decodeFromDigitsToMorse(digitSymbolsArr);
+    const humanReadableStr = decodedFromMorseToHuman(morseSymbolsArr);
+
+    function decodedFromMorseToHuman(arr) {
+        let finalStr = '';
+        
+        arr.forEach(element => {
+            let morseSymbol = '';
+
+            element.forEach(item => morseSymbol += item);
+
+            (morseSymbol === ' ') ? finalStr += ' ' : finalStr += MORSE_TABLE[morseSymbol];
+        })
+       
+        return finalStr;
+    }
+
+    function decodeFromDigitsToMorse (arr) {
+        const symbols = [];
+
+        arr.forEach(element => {
+            const symbl = [];
+
+            for(let i = 0; i < element.length; i++) {
+                if(element[i] === '1') {
+                    if(element[i + 1] === '0') {
+                        symbl.push('.');
+                        i++;
+                    }else if(element[i + 1] === '1') {
+                        symbl.push('-');
+                        i++;
+                    }
+                }else if(element[i] === '*') {
+                    symbl.push(' ');
+                    i += element.length;
+                }
+            }
+            symbols.push(symbl);
+        })
+     
+        return symbols;
+    }
+    
+    function breakExprToSymbols(Arr) {
+        const symbl = [];
+        const symbols = [];
+
+        Arr.forEach(element => {
+            symbl.push(element);
+            if(symbl.length === 10) {
+                symbols.push(symbl.splice(0, 10));
+            }
+        });
+
+        return symbols;
+    }
+
+    return humanReadableStr;
+
+    // --------- variant 1 -------------------------------
+    /*
+    const symbols = expr.split('');
+
+    let words = [];
+
+    while(symbols.length !== 0) {
+        let word = symbols.splice(0, 10);
+        words.push(word.slice(0, 10));
+        word.splice(0, 10);
+    }
+
+    function transformationToMorseWords(words) {
+        let morseWords = [];
+        words.forEach(el => {
+            const morseWord = [];
+            for(let i = 0; i < el.length; i++) {
+                if (el[i] === '1' && el[i + 1] === '0') {
+                    morseWord.push('.');
+                }else if(el[i] === '1' && el[i + 1] === '1') {
+                    morseWord.push('-');
+                    i++;
+                }else if(el[i] === '*') {
+                    morseWord.push(' ');
+                    i = el.length;
+                }
+            }
+            morseWords.push(morseWord.slice());
+        });
+        return morseWords;
+    }
+    const morseWords = transformationToMorseWords(words);
+
+    function decodedMorse(morse) {
+        let str = '';
+        morse.forEach(item => {
+            let morseSymbols = '';
+            item.forEach(sym => {
+                morseSymbols += sym;
+            })
+            str += (MORSE_TABLE[morseSymbols] === undefined) ? ' ' : MORSE_TABLE[morseSymbols];
+
+        })
+        return str;
+    }
+    const finalStr = decodedMorse(morseWords);
+    // console.log(`final string - ${finalStr}`);
+
+    return finalStr;
+    */
 }
 
 module.exports = {
